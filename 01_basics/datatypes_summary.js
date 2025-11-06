e27e12d54a9875b8f7024afb9e9b8f5a59224e63
JASHABANTAKHANDA <151468405+JASHABANTAKHANDA@users.noreply.github.com>
2025-11-06 18:46:42 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,47 @@
+// Primitive data type
+ // 7 catagories ==>String ,Number ,Boolean , null , undefined ,Symbol (use for unique) ,BigInt (big value)
+
+ const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn =false
+
+const outsideTemp = null
+
+let userEmail ; //undefined
+
+
+//symbol
+const id=Symbol('123')
+const antherId=Symbol('123')
+
+console.log(id==antherId);//false
+
+
+// const bigNumber = 5455445455777575n
+
+
+const heros = ["saktiman","naagraj", "dogo"]
+let myObj ={//under the {all are object}
+    name: "hitedh",
+    age: 22
+}
+
+//function declared usein just like veriable
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+
+//how to know data type
+console.log(typeof bigNumber);//undefined
+
+console.log(typeof outsideTemp);//object
+
+
+
+ //*********************************** */
+//Non Primitive or referance type
+/*Catagories==> Array , Object ,Functions */
+
