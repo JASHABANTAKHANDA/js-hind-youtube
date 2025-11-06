3596ccd6798db87b923a26f20fa71cd5a43a600a
JASHABANTAKHANDA <151468405+JASHABANTAKHANDA@users.noreply.github.com>
2025-11-06 17:49:01 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,3 +49,47 @@ console.log(typeof stringNumber);//string
 console.log(stringNumber);// show number (33) but typeof this number is String
 
 
+/***************Operations*************** */
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)// -3
+
+// console.log( 2 + 2);
+// console.log( 2 - 2);
+// console.log( 2 * 2);
+// console.log( 2 ** 3);//power
+// console.log( 2 / 2);
+// console.log( 2 % 2);
+
+
+// let str1="Hello"
+// let str2=" Hitesh"
+// let str3=str1+str2
+// console.log(str3); //Hello Hitesh
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+"2"+"3");
+// console.log(1+2+"3");
+
+//https://tc39.es/ecma262/#sec-toprimitive 
+/*Draft ECMA-262 / November 4, 2025
+ECMAScript® 2026 Language Specification*/
+
+console.log(+true);//possiable
+// console.log(true+);//not possiable
+// console.log(+"");//not recomend this type operation
+
+// let num1,num2 ,num3
+
+// num1 = num2 = num3 = 2+2 //it is excute but readable code is not good
+
+
+let gameCounter =100
+// gameCounter++//101 pre
+++gameCounter//101 post
+console.log(gameCounter);//101
+
+
