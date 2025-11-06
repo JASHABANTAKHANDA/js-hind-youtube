4d8b4110818a625d7f1deebc8409b9918ec30554
JASHABANTAKHANDA <151468405+JASHABANTAKHANDA@users.noreply.github.com>
2025-11-06 18:07:00 +0000

    comprision

---
+++ 01_basics/04_comprision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);//js "2" (string)convert to number
+// console.log("02" > 1);
+
+// console.log(null >0);//false
+// console.log(null == 0);//false
+// console.log(null <= 0);//true
+// console.log(null >= 0);//true
+
+// console.log(undefined ==0);//false
+// console.log(undefined >=0);//false
+// console.log(undefined <=0);//false
+
+
+// strect check mean === (value with datatype check)
+console.log("2" === 2); //false ===>//"2" //string (===) 2 //number
