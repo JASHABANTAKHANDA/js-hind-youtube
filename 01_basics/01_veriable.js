c9e940376c998a50a6db208cc517e7a101f74ec6
JASHABANTAKHANDA <151468405+JASHABANTAKHANDA@users.noreply.github.com>
2025-11-06 15:16:20 +0000

    veriable declared

---
+++ 01_basics/01_veriable.js
@@ -0,0 +1,33 @@
+const accountId =144553
+let accountEmail="mantu@gmail.com"
+/*
+var
+----------
+Prefer not to use var
+becouse of issue in block scope and function scope
+*/
+var accountPassword ="12345"
+/*
+with out veriable declared 
+we can written 
+bot this is not good practic  
+
+ accountCity = "Jaipur"
+
+*/
+accountCity = "Jaipur"
+
+/* if  declare  value not assine value js thinking undefined
+let accountState 
+
+// accountId = 2  //constant key word not allowed it is fixed value
+
+accountEmail = "hc@hc.com"
+
+accountPassword = "212121"
+
+accountCity = "Bengalure"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
