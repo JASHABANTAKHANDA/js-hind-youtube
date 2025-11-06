2e2d8c34e173a7f49a9b1204b6260a90d881167f
JASHABANTAKHANDA <151468405+JASHABANTAKHANDA@users.noreply.github.com>
2025-11-06 15:51:43 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,46 @@
+"use strict"; //treat all JS code newer version
+
+// alert( 3 + 3)  we are useing nodeJS, not browser
+
+//this line executable but rediablite is not good 
+// console.log(3 +3);console.log("Hitesh") //this line executable but rediablite is not good 
+
+console.log( 3 
+           //code reablity shoudld be high
+    + 3)
+
+
+//this is way is good
+console.log( 3 + 3)
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false  //boolean type
+let state;//undefined
+let state1= null //empty
+
+//Primitive data type
+//------------------------
+// number => 2 to power 53 (range)
+// bigint => usein tradding /stockmarket /facebook
+// string ==>use single code '' //double code ""
+// boolean ==>true /false
+// null ==> standalone value //value is empty
+// undefined ==> veriable not defined
+// symbol ==>unique 
+
+
+//Not primitive
+// object
+
+
+//typeof use for which type data
+console.log(typeof ""); //string
+console.log(typeof age); //number
+console.log(typeof null);//object
+console.log(typeof undefined) //undefined
+
+
+
+
