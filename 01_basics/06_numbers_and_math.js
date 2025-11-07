42dbe002b4d1299839765d6ef0e2b004a4c0422b
JASHABANTAKHANDA <151468405+JASHABANTAKHANDA@users.noreply.github.com>
2025-11-07 05:25:31 +0000

    numbers

---
+++ 01_basics/06_numbers_and_math.js
@@ -0,0 +1,22 @@
+//numbers
+const score = 400
+console.log(score);
+
+  const balance = new Number(100)
+  console.log(balance);
+  console.log(balance.toString());
+  console.log(balance.toString().length);
+  console.log(balance.toFixed(1))
+  
+
+  const otherNumber =123.8966
+  console.log(otherNumber.toPrecision(4))
+
+
+
+  const hunderds= 10000000
+  console.log(hunderds.toLocaleString())
+  console.log(hunderds.toLocaleString('en-IN'))
+
+
+  /**************Maths*************** */
